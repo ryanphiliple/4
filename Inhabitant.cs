89ee7898af3acf2e28d1c6b625eeaa911b5043a8
ryanphiliple <152640886+ryanphiliple@users.noreply.github.com>
2024-04-04 21:00:52 -0500

    Create Inhabitant.cs

---
+++ Inhabitant.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Inhabitant 
+{
+    protected string name;
+    protected Room currentRoom;
+    protected int hp, ac;
+
+    public Inhabitant(string s)
+    {
+        this.currentRoom = null;
+        this.name = name;
+        this.hp = Random.Range(10, 16);
+        this.ac = Random.Range(8,17);
+    }
+
+    public Room getCurrentRoom()
+    {
+        return this.currentRoom;
+    }
+
+    public void takeDamage(int damage)
+    {
+        this.hp = this.hp - damage;
+    }
+
+    public void setCurrentRoom(Room r)
+    {
+        this.currentRoom = r;
+    }
+
+    public int getHP()
+    {
+        return this.hp;
+    }
+
+    public int getAC()
+    {
+        return this.ac;
+    }
+
+}
