aa3a5036a25fae98fbe418389baf57dac45e78d6
ryanphiliple <152640886+ryanphiliple@users.noreply.github.com>
2024-04-09 21:12:15 -0500

    Added the buy item function

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject amAtMiddleOfRoom;
 
+    public TextMeshPro pellet_TMP;
     public GameObject northExit;
     public GameObject southExit;
     public GameObject eastExit;
     public GameObject westExit;
     private float speed = 5.0f;
     private bool amMoving = false;
-    public bool fightWon = false;
+    private bool amAtMiddleOfRoom = false;
+
 
     private void turnOffExits()
     {
@@ -36,6 +38,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        this.pellet_TMP.text = "" + MySingleton.currentPellets;
+
         this.turnOffExits();
 
         this.amAtMiddleOfRoom.SetActive(false);
@@ -85,96 +89,109 @@ public class PlayerController : MonoBehaviour
         }
         else if (other.CompareTag("power-pellet"))
         {
+            EditorSceneManager.LoadScene("FightScee");
+
+
             other.gameObject.SetActive(false);
 
-            if(MySingleton.thePlayer.getHP() < fightController.theMonster.getHP())
-            {
-                fightWon = true;
-                EditorSceneManager.LoadScene("Room1");
-                this.hero_hp_TMP.text = "added pellet";
-            }
-            else
-            {
-                print("you lost");
-                EditorSceneManager.LoadScene("Room1");
-            }
 
             Room theCurrentRoom = MySingleton.thePlayer.getCurrentRoom();
-            if(MySingleton.isThisTheFirstTimeInTheFirstRoom)
-            {
-                theCurrentRoom.removePellet(MySingleton.currentDirection);
-                MySingleton.isThisTheFirstTimeInTheFirstRoom = false;
-                
+            theCurrentRoom.removePellet(other.GetComponent<pelletController>().direction);
 
-            }
-            else
-            {
-                theCurrentRoom.removePellet(MySingleton.flipDirection(MySingleton.currentDirection));
-            }
-            
+
         }
         else if (other.CompareTag("MiddleOfTheRoom") && !MySingleton.currentDirection.Equals("?"))
         {
             this.MiddleOfTheRoom.SetActive(false);
             this.turnOnExits();
+
             print("at middle of the room");
             this.amAtMiddleOfTheRoom = true;
             this.amMoving = false;
             MySingleton.currentDirection = "middle";
         }
-        
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.UpArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("north"))
-        {
-            this.amMoving = true;
-            this.turnOnExits();
-            MySingleton.currentDirection = "north";
-            this.gameObject.transform.LookAt(this.northExit.transform.position);
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("south"))
-        {
-            this.amMoving = true;
-            this.turnOnExits();
-            MySingleton.currentDirection = "south";
-            this.gameObject.transform.LookAt(this.southExit.transform.position);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("west"))
-        {
-            this.amMoving = true;
-            this.turnOnExits();
-            MySingleton.currentDirection = "west";
-            this.gameObject.transform.LookAt(this.westExit.transform.position);
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("east"))
-        {
-            this.amMoving = true;
-            this.turnOnExits();
-            MySingleton.currentDirection = "east";
-            this.gameObject.transform.LookAt(this.eastExit.transform.position);
-        }
-
-        if (MySingleton.currentDirection.Equals("north"))
-        {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.northExit.transform.position, this.speed * Time.deltaTime);
-        }
-
-        if (MySingleton.currentDirection.Equals("south"))
-        {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.southExit.transform.position, this.speed * Time.deltaTime);
-        }
-
-        if (MySingleton.currentDirection.Equals("west"))
-        {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.westExit.transform.position, this.speed * Time.deltaTime);
-        }
-        if (MySingleton.currentDirection.Equals("east"))
-        {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.eastExit.transform.position, this.speed * Time.deltaTime);
-        }
-
     }
+
+    public bool BuyItem(Item item)
+    {
+        if (Pellet >= item.Cost)
+        {
+            Pellet -= item.Cost;
+            return true;
+        }
+        else
+        {
+            Debug.Log("You don't have enough gold to buy this item.");
+            return false;
+        }
+    }
+
+
+        public class Item
+        {
+          public string Name { get; }
+         public int Cost { get; }
+
+            public Item(string name, int cost)
+            {
+               Name = name;
+                Cost = cost;
+          }
+        }
+
+
+
+// Update is called once per frame
+void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.UpArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("north"))
+            {
+                this.amMoving = true;
+                this.turnOnExits();
+                MySingleton.currentDirection = "north";
+                this.gameObject.transform.LookAt(this.northExit.transform.position);
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("south"))
+            {
+                this.amMoving = true;
+                this.turnOnExits();
+                MySingleton.currentDirection = "south";
+                this.gameObject.transform.LookAt(this.southExit.transform.position);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("west"))
+            {
+                this.amMoving = true;
+                this.turnOnExits();
+                MySingleton.currentDirection = "west";
+                this.gameObject.transform.LookAt(this.westExit.transform.position);
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow) && !this.amMoving && MySingleton.thePlayer.getCurrentRoom().hasExit("east"))
+            {
+                this.amMoving = true;
+                this.turnOnExits();
+                MySingleton.currentDirection = "east";
+                this.gameObject.transform.LookAt(this.eastExit.transform.position);
+            }
+
+            if (MySingleton.currentDirection.Equals("north"))
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.northExit.transform.position, this.speed * Time.deltaTime);
+            }
+
+            if (MySingleton.currentDirection.Equals("south"))
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.southExit.transform.position, this.speed * Time.deltaTime);
+            }
+
+            if (MySingleton.currentDirection.Equals("west"))
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.westExit.transform.position, this.speed * Time.deltaTime);
+            }
+            if (MySingleton.currentDirection.Equals("east"))
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.eastExit.transform.position, this.speed * Time.deltaTime);
+            }
+
+        }
+    
 }
